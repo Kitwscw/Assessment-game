59b6ea70a1327e6dcd2f51aa8b9d59d82d3d46a2
Kitwscw <159850948+Kitwscw@users.noreply.github.com>
2024-08-22 12:29:22 +1200

    player movement mostly done

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 200
-const JUMP_VELOCITY = -400.0
+const SPEED = 150
+const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -12,7 +12,6 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -30,7 +29,9 @@ func _physics_process(delta):
 		sprite.play("idle")
 	if direction:
 		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	elif is_on_floor():
+		velocity.x = move_toward( velocity.x, 0, SPEED)
+	elif not is_on_floor():
+		velocity.x = move_toward(velocity.x, 0, 2.5)
 	move_and_slide()
+	
