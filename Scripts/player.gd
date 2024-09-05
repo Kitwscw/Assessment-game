bbb405ecf06d836897a4e4c1ca5e4f24d11bbb54
Kitwscw <159850948+Kitwscw@users.noreply.github.com>
2024-09-05 12:27:28 +1200

    camera working now (yipee)

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -6,7 +6,7 @@ const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-@onready var sprite = $AnimatedSprite2D
+@onready var sprite = $AnimatedSprite2D2
 
 func _physics_process(delta):
 	# Add the gravity.
