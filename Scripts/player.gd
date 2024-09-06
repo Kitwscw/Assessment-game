65e1be29865b4dd97b59d25765f583405d139919
Kitwscw <159850948+Kitwscw@users.noreply.github.com>
2024-09-06 12:57:23 +1200

    fall animation? (prolly not working tbh)

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -6,12 +6,13 @@ const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-@onready var sprite = $AnimatedSprite2D2
+@onready var sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		sprite.play("fall")
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
