8296c33d3dc338833fa2780d7c758c4175384116
Kitwscw <159850948+Kitwscw@users.noreply.github.com>
2024-09-13 13:46:04 +1200

    tutorial done

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,11 +3,21 @@ extends CharacterBody2D
 
 const SPEED = 150
 const JUMP_VELOCITY = -300.0
-
+const DASHSPEED = 1000
+const DASH_DURATION=0.2
+const DASH_COOLDOWN= 1.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var sprite = $AnimatedSprite2D
+@onready var dashtimer = $Timer
+
+var is_dashing = false
+var dash_time = 0.0
+
+func _ready():
+	dashtimer.wait_time = DASH_COOLDOWN
+	dashtimer.one_shot = true
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -21,6 +31,8 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("move left", "move right")
+	var isdash = Input.is_action_just_pressed("dash")
+			
 	if direction<0:
 		sprite.flip_h = true
 		sprite.play("run")
